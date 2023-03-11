7921a04724264626b3ac194ac3043daf02b7055b
mrtnprzk <marti.parizek@gmail.com>
2023-03-11 15:12:07 +0100

    [UPDATE] Head metadata moved into AppLayout

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,4 +1,6 @@
 import { FC, ReactNode } from "react";
+import Head from "next/head";
+
 import Header from "../Header/Header";
 
 interface AppLayoutProps {
@@ -7,12 +9,23 @@ interface AppLayoutProps {
 
 const AppLayout: FC<AppLayoutProps> = ({ children }) => {
   return (
-    <main className="flex flex-col w-full">
-      <Header />
-      <main className="container mx-auto px-2 mt-4 md:mt-8 lg:mt-12 xl:mt-16">
-        {children}
+    <>
+      <Head>
+        <title>Blog Cat App</title>
+        <meta
+          name="description"
+          content="Created by @mrtnprzk for Applifting"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex flex-col w-full">
+        <Header />
+        <main className="container mx-auto px-2 mt-4 md:mt-8 lg:mt-12 xl:mt-16">
+          {children}
+        </main>
       </main>
-    </main>
+    </>
   );
 };
 
